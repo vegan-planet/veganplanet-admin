addaf4f7e1ff04eaf2f491fc9d9eba3060836db3
王小欢 <wxh_work@163.com>
2023-12-19 16:49:53 +0800

    feat: 集成element-plus，src文件夹别名配置

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
-
-const fun = () => {
-  console.log('sdfsdf')
-}
-fun()
+//引入element-plus插件和样式
+import ElementPlus from 'element-plus'
+//引入国际化插件
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+//引入element-plus,css样式
+import 'element-plus/dist/index.css'
+//获取应用实例对象
+const app = createApp(App)
+//安装element-plus插件  一定要在use(router)之前挂载
+app.use(ElementPlus, { locale: zhCn })
+//将应用实例挂载到#app上
+app.mount('#app')
