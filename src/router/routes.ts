edbed7fbe72a4658ce319d49e28ed01cb960e4d5
王小欢 <wxh_work@163.com>
2023-12-22 12:29:57 +0800

    feat: 封装路由模版

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,27 +1,27 @@
 //对外暴露配置路由(常量路由):全部用户都可以访问到的路由
 export const constantRoute = [
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('@/views/login/login.vue'),
-        meta: {
-            title: '素食星球登录页'
-        }
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/login.vue'),
+    meta: {
+      title: '素食星球登录页',
     },
-    {
-        path: '/',
-        name: 'Home',
-        component: () => import('@/views/home/home.vue'),
-        meta: {
-            title: '素食星球登首页'
-        }
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/home/home.vue'),
+    meta: {
+      title: '素食星球登首页',
     },
-    {
-        path: '/:pathMatch(.*)*',
-        name: 'NotFound',
-        component: () => import('@/views/error/error.vue'),
-        meta: {
-            title: '404'
-        }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/views/error/error.vue'),
+    meta: {
+      title: '404',
     },
+  },
 ]
